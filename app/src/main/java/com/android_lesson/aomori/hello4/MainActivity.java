9f3acc2c97bdbf068dad05e4b7de2d9122cb2b64
Atsushi Kokubo <kokubo@aomori-u.ac.jp>
2015-11-17 09:39:32 +0900

    Activityのライフサイクルイベントの呼び出しをログ出力

--- app/src/main/java/com/android_lesson/aomori/hello4/MainActivity.java
+++ app/src/main/java/com/android_lesson/aomori/hello4/MainActivity.java
@@ -3,6 +3,7 @@ package com.android_lesson.aomori.hello4;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,6 +13,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Log.d("TAG", "onCreateが実行されました");
+
         setContentView(R.layout.activity_main);
 
         mPlayer = MediaPlayer.create(getApplicationContext(), R.raw.amclassical_jesu_joy_of_mans_desiring);
@@ -39,30 +43,42 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
+
+        Log.d("TAG", "onDestroyが実行されました");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
+
+        Log.d("TAG", "onPauseが実行されました");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
+
+        Log.d("TAG", "onRestartが実行されました");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        Log.d("TAG", "onResumeが実行されました");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
+
+        Log.d("TAG", "onStopが実行されました");
     }
 
     @Override
     protected void onStart() {
         super.onStart();
+
+        Log.d("TAG", "onStartが実行されました");
     }
 }
